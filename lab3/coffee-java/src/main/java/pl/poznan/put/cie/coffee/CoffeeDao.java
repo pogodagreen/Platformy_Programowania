72d3326e962b4840084c3c3272aa975fa530e8c7
pogodagreen <pogodagreen@gmail.com>
2019-10-27 20:32:43 +0100

    dao

--- lab3/coffee-java/src/main/java/pl/poznan/put/cie/coffee/CoffeeDao.java
+++ lab3/coffee-java/src/main/java/pl/poznan/put/cie/coffee/CoffeeDao.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -18,9 +19,7 @@ public class CoffeeDao {
 	private final NamedParameterJdbcTemplate jdbc;
 
 	public CoffeeDao() {
-		// TODO init jdbc using pl.poznan.put.cie.coffee.DbUtilities
-		this.jdbc = null;
-		throw new UnsupportedOperationException("Not implemented yet.");
+		this.jdbc = new NamedParameterJdbcTemplate(DbUtilities.getDataSource("jdbc:derby://localhost:1527/lab"));
 	}
 
 	/**
@@ -38,8 +37,19 @@ public class CoffeeDao {
 			@Override
 			public Coffee extractData(ResultSet rs) throws SQLException, DataAccessException {
 				// TODO implement method #extractData()
-				throw new UnsupportedOperationException("Not implemented yet.");
-			}
+				Coffee coffee=new Coffee();
+                while(rs.next())
+				{
+
+					coffee.setName(rs.getString("COF_NAME"));
+					coffee.setSupplierId(rs.getInt("SUP_ID"));
+					coffee.setPrice(rs.getBigDecimal("PRICE"));
+					coffee.setSales(rs.getInt("SALES"));
+					coffee.setTotal(rs.getInt("TOTAL"));
+
+				}
+				return coffee;
+		}
 		});
 	}
 
@@ -51,8 +61,28 @@ public class CoffeeDao {
 	public List<Coffee> getAll() {
 		String sql = "SELECT cof_name, sup_id, price, sales, total FROM coffees";
 		try {
+                    return jdbc.query(sql, new ResultSetExtractor<List<Coffee>>() {
+
+			@Override
+			public List<Coffee> extractData(ResultSet rs) throws SQLException, DataAccessException {
+				// TODO implement method #extractData()
+				List<Coffee> list=new ArrayList<>();
+                while(rs.next())
+				{
+                                    Coffee coffee=new Coffee();
+
+					coffee.setName(rs.getString("COF_NAME"));
+					coffee.setSupplierId(rs.getInt("SUP_ID"));
+					coffee.setPrice(rs.getBigDecimal("PRICE"));
+					coffee.setSales(rs.getInt("SALES"));
+					coffee.setTotal(rs.getInt("TOTAL"));
+                                        list.add(coffee);
+				}
+				return list;
+                        }
+                    });
 			// TODO invoke NamedParameterJdbcTemplate.query(String, RowMapper<T>)
-			throw new UnsupportedOperationException("Not implemented yet.");
+		//	throw new UnsupportedOperationException("Not implemented yet.");
 		} catch (EmptyResultDataAccessException ex) {
 			return new ArrayList<>();
 		}
@@ -72,15 +102,27 @@ public class CoffeeDao {
 	}
 
 	public void delete(String name, int supplierId) {
-		// TODO implement method CoffeeDao#delete(), use NamedParameterJdbcTemplate.update(String, Map)
-		// DELETE FROM coffees WHERE cof_name = ? AND sup_id = ?
-		throw new UnsupportedOperationException("Not implemented yet.");
+		String sql = "DELETE FROM coffees "
+				  + "WHERE cof_name = :cof_name AND sup_id = :sup_id";
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("cof_name", name);
+		parameters.put("sup_id", supplierId);
+		jdbc.update(sql, parameters);
 	}
 
 	public void create(Coffee c) {
+                String sql = "INSERT INTO coffees(cof_name, sup_id, price, sales, total) "
+				  + "VALUES( :cof_name, :sup_id, :price, :sales, :total)";
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("cof_name", c.getName());
+		parameters.put("sup_id", c.getSupplierId());
+                parameters.put("price", c.getPrice());
+		parameters.put("sales", c.getSales());
+		parameters.put("total", c.getTotal());
+		jdbc.update(sql, parameters);
 		// TODO implement method CoffeeDao#create(), use NamedParameterJdbcTemplate.update(String, Map)
 		// INSERT INTO coffees(cof_name, sup_id, price, sales, total) VALUES(?, ?, ?, ?, ?)
-		throw new UnsupportedOperationException("Not implemented yet.");
+		//throw new UnsupportedOperationException("Not implemented yet.");
 	}
 
 }
